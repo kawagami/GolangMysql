489150843e131f3bfa172d9a3dcd67909b7d2668
Kawa <joelai1988@gmail.com>
2022-04-29 16:19:20 +0800

    簡單測試 goroutine & channel 完成模型

--- main.go
+++ main.go
@@ -3,51 +3,38 @@ package main
 import (
 	// "fmt"
 	"fmt"
-	"mods/multiExam"
+	"time"
 )
 
 const (
 	StringForNow = "2006-01-02 15:04:05"
 )
 
+func addData(dataChan chan int) {
+	for i := 0; i < 50; i++ {
+		fmt.Printf("addData 第 %v 次\n", i+1)
+		dataChan <- i
+	}
+	fmt.Println("關閉 dataChan")
+	close(dataChan)
+}
+
 func main() {
-	// 暫時儲存資料用的 channel 長度"目測"是影響不大
-	var numChan = make(chan int, 100)
-	// 處理過資料的儲存 channel ，如果開多個 goroutine 同時寫入的話要考慮關閉的時間點
-	var resChan = make(chan map[int]int, 2000)
-	// 用來記錄開啟的 goroutine 結束的 channel
-	var exitChan = make(chan bool, 8)
-	// 建立初始資料
-	go multiExam.CreateNum(numChan)
-	// 開啟八個 go 處理初始資料
-	for i := 0; i < 8; i++ {
-		go multiExam.Calculate(numChan, resChan, exitChan)
-	}
-	// 使用匿名函數包裝邏輯，取出特定數量的資料
+	var dataChan = make(chan int, 10)
+
+	go addData(dataChan)
+
 	func() {
-		// 沒取出設定數量的資料的話似乎就會停在這邊
-		for i := 0; i < 8; i++ {
-			// 不需要取出的資料，就丟去虛空
-			<-exitChan
+		for {
+			v, ok := <-dataChan
+			if !ok {
+				fmt.Println("沒東西了")
+				break
+			}
+			fmt.Println("取得", v)
+			time.Sleep(time.Millisecond * 10)
 		}
-		fmt.Println("取出 8 個了")
-		// 取出設定數量的話就能把紀錄處理後資料的 channel 關閉
-		close(resChan)
 	}()
-	// 整理 resChan 的資料
-	// 整理這邊的邏輯應該可以使用 goroutine 處理，待學習
-	var result = make(map[int]int, 2000)
-	for {
-		res, ok := <-resChan
-		if !ok {
-			break
-		}
-		for i, v := range res {
-			result[i] = v
-		}
-	}
-	for i := 0; i < 2000; i++ {
-		fmt.Printf("res[%v] = %v\n", i, result[i])
-	}
-	fmt.Println("main 結束")
+
+	fmt.Println("main end")
 }
