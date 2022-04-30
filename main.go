d04c28110ac1ad3ab19d893be26b0f7e39bb676e
Kawagami <joelai1988@gmail.com>
2022-04-30 22:06:18 +0800

    可滿足依照條件壓縮檔案的版本

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	zip "mods/zipDir"
+	"os"
 	"time"
 )
 
 const (
-	stringForNow = "2006-01-02 15:04:05"
-	root         = `D:\temp`
+	// root           = `D:\temp`
+	stringForNow   = "2006-01-02 15:04:05"
+	root           = `C:\test`
+	handleChannels = 10
 )
 
 func putDataIntoChannel(files map[string]string, newChan chan map[string]string) {
@@ -25,11 +29,59 @@ func handle(newChan chan map[string]string, exitChan chan bool) {
 			break
 		}
 		for fileName, filePath := range v {
-			fmt.Printf("取出 %v 了\nPath = %v\n\n", fileName, filePath)
+			// 檢查在 root 是否有同名壓縮檔
+			sameName := filePath + ".zip"
+			if _, err := os.Stat(sameName); os.IsNotExist(err) {
+				// 沒同名壓縮檔的處理
+				// 檢查資料夾下是圖片還是壓縮檔
+				zipFileName := filePath + ".zip"
+				// 這邊先偷懶，檔案數不等於 1 的都當是圖片檔
+
+				if zfn, isZipFile := isZipFile(filePath); isZipFile {
+					oldPath := filePath + `\` + zfn
+					newPath := filePath + `.zip`
+					fmt.Printf("move %v\n", fileName)
+					os.Rename(oldPath, newPath)
+				} else {
+					// 壓縮檔案的處理
+					fmt.Printf("zip %v\n", fileName)
+					zip.ZipSource(filePath, zipFileName)
+				}
+				// 壓縮完後移除資料夾
+				checkExist(zipFileName, filePath)
+			} else {
+				// 有同名壓縮檔的處理
+				fmt.Printf("有同名的壓縮檔 %v\n", fileName)
+			}
 		}
-		time.Sleep(time.Millisecond * 100)
 	}
+	fmt.Println("one goroutine done!")
 	exitChan <- true
+}
+
+func isZipFile(path string) (string, bool) {
+	info, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Printf("%v\n\n", info[0].Name())
+	// fmt.Printf("有 %v 個檔案", len(info))
+	res1 := info[0].Name()
+	res2 := len(info) == 1
+	return res1, res2
+}
+
+func checkExist(filePath, dirPath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// 沒該檔案的動作
+	} else {
+		// 有該檔案的動作
+		err := os.RemoveAll(dirPath)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("delete %v\n", dirPath)
+	}
 }
 
 func main() {
@@ -37,16 +89,22 @@ func main() {
 	//
 	files := zip.GetDirPath(root)
 	//
+	timeRecordStart := time.Now().UnixMicro()
 	var newChan = make(chan map[string]string, 100)
 	go putDataIntoChannel(files, newChan)
-	var exitChan = make(chan bool, 4)
-	for i := 0; i < 4; i++ {
+	var exitChan = make(chan bool, handleChannels)
+	for i := 0; i < handleChannels; i++ {
 		go handle(newChan, exitChan)
 	}
 	//
-	for i := 0; i < 4; i++ {
+	for i := 0; i < handleChannels; i++ {
 		<-exitChan
 	}
+	timeRecordEnd := time.Now().UnixMicro()
+	//
+	timeRecordMicro := timeRecordEnd - timeRecordStart
+	timeRecordSecond := float64(timeRecordMicro) / 1000000
+	fmt.Printf("經過 %v 秒\n", timeRecordSecond)
 	//
 	fmt.Println("main end")
 }
