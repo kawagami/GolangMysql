02631db9820e69c90b04f16d6b223a9754e8ce91
Kawagami <joelai1988@gmail.com>
2022-05-01 11:17:19 +0800

    整理變數

--- main.go
+++ main.go
@@ -2,32 +2,27 @@ package main
 
 import (
 	"fmt"
+	"mods/setting"
 	zip "mods/zipDir"
 	"time"
 )
 
-const (
-	root           = `D:\temp`
-	stringForNow   = "2006-01-02 15:04:05"
-	handleChannels = 10
-)
-
 func main() {
-	fmt.Println(time.Now().Format(stringForNow))
+	fmt.Println(time.Now().Format(setting.TIMENOW))
 	// 取得要整理的子資料夾 map
-	files := zip.GetDirPath(root)
+	files := zip.GetDirPath(setting.TEMPPATH)
 	//
 	timeRecordStart := time.Now().UnixMicro()
 	// 將資料放入 channel
 	var newChan = make(chan map[string]string, 100)
 	go zip.PutDataIntoChannel(files, newChan)
 	// 把 channel 中的資料取出並處理
-	var exitChan = make(chan bool, handleChannels)
-	for i := 0; i < handleChannels; i++ {
+	var exitChan = make(chan bool, setting.HANDLECHANNELS)
+	for i := 0; i < setting.HANDLECHANNELS; i++ {
 		go zip.Handle(newChan, exitChan)
 	}
 	// 等待開啟的 goroutine 都結束
-	for i := 0; i < handleChannels; i++ {
+	for i := 0; i < setting.HANDLECHANNELS; i++ {
 		<-exitChan
 	}
 	timeRecordEnd := time.Now().UnixMicro()
---
+++ setting/baseSetting.go
@@ -0,0 +1,11 @@
+package setting
+
+const (
+	TEMPPATH       = `D:\temp`
+	TIMENOW        = "2006-01-02 15:04:05"
+	SQLUSER        = "root"
+	SQLPW          = "root"
+	SQLIP          = "127.0.0.1:3306"
+	SQLDB          = "arrange"
+	HANDLECHANNELS = 10
+)
