e71b84ba0b57397db1556929db3f82b98d016c89
Kawagami <joelai1988@gmail.com>
2022-05-03 23:46:48 +0800

    爬蟲取資料加入 regex 過濾字串

--- crawler/actressName.go
+++ crawler/actressName.go
@@ -2,6 +2,7 @@ package crawler
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 
 	"mods/GetInfo"
@@ -18,11 +19,16 @@ func CrawlerActressName() {
 	//
 	// 取得 DB 資料
 	var va mysql.VideoActresses
+	// regex
+	pattern := `^[a-zA-Z]{2,6}-[0-9]{2,6}`
+	re, _ := regexp.Compile(pattern)
+	//
 	for _, video := range pathSlice {
-		if !va.Exist(video.Title) {
+		regexTitle := re.FindString(video.Title)
+		if !va.Exist(regexTitle) && regexTitle != "" {
 			// 取得名字
-			var insert = mysql.VideoActresses{Title: video.Title}
-			if actressName := GetActressName(video.Title); actressName != "" {
+			var insert = mysql.VideoActresses{Title: regexTitle}
+			if actressName := GetActressName(regexTitle); actressName != "" {
 				// 建立要插入 DB的資料
 				insert.Actress = actressName
 				// 將資料寫入 DB
@@ -31,12 +37,12 @@ func CrawlerActressName() {
 				fmt.Println("查無 actress name")
 			}
 			va.Insert(insert)
-			fmt.Println("寫入", video.Title)
+			fmt.Println("寫入", regexTitle)
 			// 避免過度 request 被擋
 			fmt.Println("待機 5 秒")
 			time.Sleep(time.Second * 5)
 		} else {
-			fmt.Println("DB 有", video.Title, "的資料")
+			fmt.Println("DB 有", regexTitle, "的資料")
 		}
 	}
 }
